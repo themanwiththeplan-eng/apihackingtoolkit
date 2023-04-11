7d84ba11770bff2370e243ee60867de88fedf146
Dakota Dunn <dakotadunn5@gmail.com>
2023-04-11 18:25:55 -0400

    worked out some kinks

--- toolset.py
+++ toolset.py
@@ -9,29 +9,47 @@ import pandas as pd
 
 
 parser = args.ArgumentParser(description="Insert arguments for API hacking toolsuite")    
-parser.add_argument('-w', '--wordlist')
 parser.add_argument('-d', '--domain')
 parser.add_argument('-s', '--subdomain')
 parser.add_argument('-dir', '--directories')
 parser.add_argument('-v','--verbose', action="store_true")
+parser.add_argument('-c', '--curl')
 parser.add_argument('-done')
 arguments = parser.parse_args()
 
 def startProgram():
-    if(arguments.domain):
-        response = rq.get(arguments.domain)
-        print(response)
-    elif (arguments.domain, arguments.wordlist):
-        file = open(arguments.wordlist, "r")
-        lines = file.readlines()
-        for l in lines:
-            get = rq.get(arguments.domain + "/" + l)
-            post = rq.post(arguments.domain + "/" + l)
-            getResponse = get.json
-            postResponse = post.json
-            print("GET:"f"{getResponse}")
-            print("POST:",f"{postResponse}")
-    
+    if(arguments.curl):
+        response = rq.get(arguments.curl)
+        print(response.text)
+        status = response.status_code
+        if (status >= 200 and status <= 400):
+            print (status)
+        elif(status >= 400):
+            print(f"{status}", "is not okay")
+    elif (arguments.domain):
+        if(arguments.directories):
+            file = open(arguments.directories, "r")
+            lines = file.readlines()
+            for l in lines:
+                get = rq.get(arguments.domain + "/" + l)
+                post = rq.post(arguments.domain + "/" + l)
+                getResponse = get.json
+                postResponse = post.json
+                print("GET:", f"{getResponse}")
+                print("POST:",f"{postResponse}")
+        elif(arguments.subdomain):
+            file = open(arguments.subdomain, "r")
+            lines = file.readlines()
+            for l in lines:
+                l = l.strip("\n")
+                url = "https://" + l + "." + arguments.domain
+                get = rq.get(url)
+                post = rq.post(url)
+                getResponse = get.status_code
+                postRes = post.status_code
+                print ("GET:", f"{getResponse}")
+                print("POST:", f"{postRes}")
+
         
 
 
