6593c8b52e064e816ef0667809a00a9066f25525
Dakota Dunn <dakotadunn5@gmail.com>
2023-04-12 20:33:53 -0400

    worked on GUI and finished original functionality associated with dir/sub bruteforce and enumeration

--- toolset.py
+++ toolset.py
@@ -15,12 +15,37 @@ parser.add_argument('-s', '--subdomain')
 parser.add_argument('-dir', '--directories')
 parser.add_argument('-v','--verbose', action="store_true")
 parser.add_argument('-c', '--curl')
+parser.add_argument('-g', '--gui')
 parser.add_argument('-done')
 arguments = parser.parse_args()
 
+
+
+def startGui():
+    root =Tk()
+    root.title("hAPI Toolkit :D")
+    root.configure(background="Black")
+    root.minsize(200, 200)
+    root.maxsize(500, 500)
+    root.geometry("300x300+50+50")
+    Label(root, text="Domain", bg="black").grid(row=0, column=1, padx=5, pady=5, sticky=E)
+    domain = Entry(root, bd=3)
+    domain.grid(row=0, column=2, columnspan=4, padx=5, pady=5)
+    runDir = Button(root, text="Directory BruteForce")
+    runDir.grid(row=2, column=2, columnspan=4, padx=5, pady=5)
+    runSub = Button(root, text="Subdomain Enumeration")
+    runSub.grid(row=3, column=2, columnspan=4, padx=5, pady=5)
+    quit = Button(root, text="Quit", command=root.quit)
+    quit.grid(row=4, column=2, columnspan=4, padx=5, pady=5)
+    root.mainloop()
+
+# startGui()
+
+
+
 def startProgram():
     if(arguments.curl):
-        response = rq.get(arguments.curl)
+        response = rq.get("https://" + arguments.curl)
         print(response.text)
         status = response.status_code
         if (status >= 200 and status <= 400):
@@ -32,8 +57,8 @@ def startProgram():
             file = open(arguments.directories, "r")
             lines = file.readlines()
             for l in lines:
-                get = rq.get(arguments.domain + "/" + l)
-                post = rq.post(arguments.domain + "/" + l)
+                get = rq.get('https://' + arguments.domain + "/" + l)
+                post = rq.post('https://' + arguments.domain + "/" + l)
                 getResponse = get.json
                 postResponse = post.json
                 print("GET:", f"{getResponse}")
@@ -46,24 +71,19 @@ def startProgram():
                 url = "https://" + l + "." + arguments.domain
                 get = rq.get(url)
                 post = rq.post(url)
-                getResponse = get.status_code
+                getRes = get.status_code
                 postRes = post.status_code
-                print ("GET:", f"{getResponse}")
-                print("POST:", f"{postRes}")
+                print ("GET:", f"{url}", f"{getRes}")
+                print("POST:", f"{url}", f"{postRes}")
 
         
 
 
 
+# TODO: Add more functionality to curl
+# TODO: Add more to the gui and get it to work when an argument is passed 
 
 
-startProgram()
+# startProgram()
 
-def startGui():
-    root =Tk()
-    root.title("hAPI Toolkit :D")
-    root.configure(background="Black")
-    root.minsize(200, 200)
-    root.maxsize(500, 500)
-    root.geometry("300x300+50+50")
-    root.mainloop()
+
