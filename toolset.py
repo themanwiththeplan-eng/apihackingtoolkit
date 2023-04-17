2c5583837d97d95bd9610af5bc5dcfe368d727f4
Dakota Dunn <dakotadunn5@gmail.com>
2023-04-17 19:41:54 -0400

    worked on the gui side of the application to be able to effectively use the gui to do subdomain enumeration, will create an upload function at some point

--- toolset.py
+++ toolset.py
@@ -45,6 +45,18 @@ def startGui():
     root.minsize(200, 200)
     root.maxsize(500, 500)
     root.geometry("300x300+50+50")
+    def enumerate_subdomains():
+        global entry
+        domain1 = domain.get()
+        entryfile1 = entryfile.get()
+        file = open(entryfile1, "r")
+        lines = file.readlines()
+        for l in lines:
+           l = l.strip("\n")
+           url = "https://" + l + "." + domain1
+           get = rq.get(url)
+           getRes = get.status_code
+           print(url + " " + str(getRes))
     Label(root, text="Domain", bg="black").grid(row=0, column=1, padx=5, pady=5, sticky=E)
     domain = Entry(root, bd=3)
     domain.grid(row=0, column=2, columnspan=4, padx=5, pady=5)
@@ -53,8 +65,10 @@ def startGui():
     entryfile.grid(row=1, column=2, columnspan=4, padx=5, pady=5)
     runDir = Button(root, text="Directory BruteForce")
     runDir.grid(row=2, column=2, columnspan=4, padx=5, pady=5)
-    runSub = Button(root, text="Subdomain Enumeration")
+    runSub = Button(root, text="Subdomain Enumeration", command=enumerate_subdomains)
     runSub.grid(row=3, column=2, columnspan=4, padx=5, pady=5)
+    label = Label(root, text="", font=("Courier 22 bold"))
+    label.grid(row=20, column=2, columnspan=4, padx=5, pady=5)
     quit = Button(root, text="Quit", command=root.quit)
     quit.grid(row=4, column=2, columnspan=4, padx=5, pady=5)
     root.mainloop()
